c118e7526f0f8d2ca8f75bf3243f5e00c2c8ea7b
mfaizanchaki <faizanchaki.mfc@gmail.com>
2017-03-27 11:37:25 +0500

    Added functionality for creating new blog posts and styling

--- app.js
+++ app.js
@@ -3,3 +3,58 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 var app = express();
+
+// APP CONFIG
+mongoose.connect('mongodb://localhost/restful_blog_app');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+// MONGOOSE/MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model('Blog', blogSchema);
+
+// ROUTES
+app.get('/', function(req, res){
+    res.redirect('/blogs');
+});
+
+app.get('/blogs', function(req, res){
+    Blog.find({}, function(err, blogs){
+        if(err){
+            throw err;
+        }else{
+            res.render('index', {blogs: blogs});
+        }
+    });
+});
+
+app.get('/blogs/new', function(req, res){
+    res.render('newpost');
+});
+
+app.post('/blogs', function(req, res){
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            res.render('newpost');
+        }else{
+            res.redirect('/blogs');
+        }
+    });
+});
+
+// title
+// image
+// content
+// created
+
+app.listen(3000, function(){
+    console.log('Server is running');
+})
