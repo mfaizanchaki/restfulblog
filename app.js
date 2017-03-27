adc4e8befe2d50f0367126339d3cdbfbc4503ffc
mfaizanchaki <faizanchaki.mfc@gmail.com>
2017-03-27 15:19:28 +0500

    Added functionality to edit posts

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 var mongoose = require('mongoose');
 
 var app = express();
@@ -9,6 +10,7 @@ mongoose.connect('mongodb://localhost/restful_blog_app');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 
 // MONGOOSE/MODEL CONFIG
@@ -59,6 +61,28 @@ app.get('/blogs/:id', function(req, res){
             res.redirect('/blogs');
         }else{
             res.render('show', {blog: blog});
+        }
+    });
+});
+
+// Edit Route
+app.get('/blogs/:id/edit', function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect('/blogs');
+        }else{
+            res.render('edit', {blog: foundBlog});
+        }
+    });
+});
+
+// Update Route
+app.put('/blogs/:id', function(req, res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err) {
+            res.redirect('/blogs');
+        }else{
+            res.redirect('/blogs/' + req.params.id);
         }
     });
 });
